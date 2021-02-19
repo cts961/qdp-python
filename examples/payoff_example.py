59f59deecf7fc060723a972a73307571f1c087e9
Liao Song <songliao@foxmail.com>
2021-02-19 23:03:26 +0800

    check the new payoff class

--- examples/payoff_example.py
+++ examples/payoff_example.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from modules.payoff import *
 
 # synthetic payoff construction
+
 p1 = VanillaPayoff(PayoffType.Call, 1)
 p2 = VanillaPayoff(PayoffType.Call, 1.15)
 
@@ -10,13 +11,29 @@ p3 = BarrierPayoff(PayoffType.Put, BarrierType.DownOut, 0.9, 1.0)
 
 p4 = VanillaPayoff(PayoffType.Put, 0.9)
 
-p = p1 - p2 + p3 - p4
+
+class Coupon:
+    def __init__(self, d0, r):
+        self.d0 = d0
+        self.r = r
+
+    def __getitem__(self, d1):
+        return (d1 - self.d0) * self.r
+
+
+cash = Coupon(0, 0.2)
+# p5 has a subscriptable arguments, make p5 pay function has an extra dependent variable t
+p5 = CashOrNothingPayoff(PayoffType.Call, 1.0, cash)
+
+# p contains p5
+# so p pay function has the form of pay(s, t)
+p = p1 - p2 + p3 - p4 + p5
 
 s = np.linspace(0.5, 1.5, 1000)
 payoff = []
 
 for x in s:
-    payoff.append(p.pay(x))
+    payoff.append(p.pay(x, 1))
 
 plt.plot(s, payoff)
 plt.show()
