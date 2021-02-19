ca73ba3de34495f70d320506dc1d26faa2422253
Liao Song <songliao@foxmail.com>
2021-02-19 23:03:08 +0800

    make the payoff class supports time dependent parameters
    if the payoff class parameters is subscriptable, the pay method has the form pay(spot, t), so that the payoff is now time dependent

--- modules/payoff.py
+++ modules/payoff.py
@@ -7,21 +7,31 @@ class PayoffType(enumerate):
     Put = -1.0
 
 
+def getvalue(k, t):
+    if hasattr(k, "__getitem__"):
+        if t is not None:
+            return k[t]
+        else:
+            raise IndexError
+
+    return k
+
+
 class Payoff:
     def __init__(self, payoff_func):
         self.payoff_func = payoff_func
 
-    def pay(self, spot: float) -> float:
-        return self.payoff_func(spot)
+    def pay(self, spot: float, t=None) -> float:
+        return self.payoff_func(spot, t)
 
     def __neg__(self):
-        return Payoff(lambda s: -self.payoff_func(s))
+        return Payoff(lambda s, t: -self.payoff_func(s, t))
 
     def __add__(self, other):
-        return Payoff(lambda s: self.payoff_func(s) + other.payoff_func(s))
+        return Payoff(lambda s, t: self.payoff_func(s, t) + other.payoff_func(s, t))
 
     def __sub__(self, other):
-        return Payoff(lambda s: self.payoff_func(s) - other.payoff_func(s))
+        return Payoff(lambda s, t: self.payoff_func(s, t) - other.payoff_func(s, t))
 
 
 class VanillaPayoff(Payoff):
@@ -31,16 +41,10 @@ class VanillaPayoff(Payoff):
         self._participation_rate = participation_rate
         super().__init__(self.pay)
 
-    @property
-    def strike(self):
-        return self._strike
-
-    @strike.setter
-    def strike(self, strike):
-        self._strike = strike
-
-    def pay(self, spot: float):
-        return max((spot - self._strike) * self._payoff_type, 0) * self._participation_rate
+    def pay(self, spot: float, t=None):
+        k = getvalue(self._strike, t)
+        pr = getvalue(self._participation_rate, t)
+        return max((spot - k) * self._payoff_type, 0) * pr
 
 
 class CashOrNothingPayoff(Payoff):
@@ -50,29 +54,21 @@ class CashOrNothingPayoff(Payoff):
         self._cash_amount = cash_amount
         super().__init__(self.pay)
 
-    @property
-    def strike(self):
-        return self._strike
-
-    @strike.setter
-    def strike(self, strike):
-        self._strike = strike
-
-    @property
-    def cash(self):
-        return self._cash_amount
-
-    @cash.setter
-    def cash(self, cash_amount):
-        self._cash_amount = cash_amount
-
-    def pay(self, spot):
-        return np.sign(max((spot - self._strike) * self._payoff_type, 0)) * self._cash_amount
+    def pay(self, spot, t=None):
+        k = getvalue(self._strike, t)
+        c = getvalue(self._cash_amount, t)
+        return np.sign(max((spot - k) * self._payoff_type, 0)) * c
 
 
 class AssetOrNothingPayoff(Payoff):
-    def __init__(self, payoff_type: PayoffType, strike: float):
-        super().__init__(lambda s: np.sign(max((s - strike) * payoff_type, 0)) * s)
+    def __init__(self, payoff_type: PayoffType, strike):
+        self._strike = strike
+        self._payoff_type = payoff_type
+        super().__init__(self.pay)
+
+    def pay(self, spot, t=None):
+        k = getvalue(self._strike, t)
+        return np.sign(max((spot - k) * self._payoff_type, 0)) * spot
 
 
 class BarrierPayoff(Payoff):
@@ -82,17 +78,19 @@ class BarrierPayoff(Payoff):
         self.strike = strike
         self.barrier = barrier
         self._participation_rate = participation_rate
-        super().__init__(self.payoff_func)
+        super().__init__(self.pay)
 
-    def barrier_factor(self, spot: float):
-        if (self.barrier_type == BarrierType.UpOut and spot <= self.barrier
-                or self.barrier_type == BarrierType.UpIn and spot > self.barrier
-                or self.barrier_type == BarrierType.DownIn and spot < self.barrier
-                or self.barrier_type == BarrierType.DownOut and spot >= self.barrier):
+    def barrier_factor(self, spot: float, t=None):
+        b = getvalue(self.barrier, t)
+
+        if (self.barrier_type == BarrierType.UpOut and spot <= b
+                or self.barrier_type == BarrierType.UpIn and spot > b
+                or self.barrier_type == BarrierType.DownIn and spot < b
+                or self.barrier_type == BarrierType.DownOut and spot >= b):
             return 1.0
         return 0
 
-    def payoff_func(self, spot: float):
-        bf = self.barrier_factor(spot)
+    def pay(self, spot: float, t=None):
+        bf = self.barrier_factor(spot, t)
         vp = VanillaPayoff(self.payoff_type, self.strike, self._participation_rate)
-        return bf * vp.pay(spot)
+        return bf * vp.pay(spot, t)
