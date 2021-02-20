8959d381a553decff09b724bc7f40a628d786a86
Liao Song <songliao@foxmail.com>
2021-02-20 14:10:02 +0800

    more flexible barrier creation routine

--- modules/barrier.py
+++ modules/barrier.py
@@ -14,12 +14,33 @@ class BarrierType(enumerate):
     DownIn = 4
 
 
+'''
+create a dictionary according to the key, 
+if the number of values is less then the number of keys, the last value will be repeated to pair with the keys
+'''
+
+
+def create_dict(keys: List, values) -> dict:
+    gen_dict = {}
+    if hasattr(values, "__iter__"):
+        if len(values) >= len(keys):
+            for i in range(len(keys)):
+                gen_dict[keys[i]] = values[i]
+        else:
+            for i in range(len(values)):
+                gen_dict[keys[i]] = values[i]
+            for i in range(len(values), len(keys)):
+                gen_dict[keys[i]] = values[- 1]
+    else:
+        for i in range(len(keys)):
+            gen_dict[keys[i]] = values
+    return gen_dict
+
+
 class Barrier:
 
-    def __init__(self, observation_dates: List[Date], barriers: List[float], barrier_type: BarrierType):
-        self._observation_dates = observation_dates
-        self._barriers = barriers
-        self._barrier_dict = dict(zip(observation_dates, barriers))
+    def __init__(self, observation_dates: List[Date], barrier_values, barrier_type: BarrierType):
+        self._barrier_dict = create_dict(observation_dates, barrier_values)
         self._barrier_type = barrier_type
 
     def is_hit(self, date: Date, spot: float) -> bool:
